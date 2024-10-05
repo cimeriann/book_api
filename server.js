7bbe2273e148ca35556742a313e5af6b56e9e971
Adebisi Olayinka <adebisiolayinka30@gmail.com>
2024-10-05 13:14:23 +0100

    use middlewares

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const connectDb = require('./config/db');
 const logger = require('./utils/logger');
+const errorHandler = require('./middleware/errorhandler/index');
+const bookRouter = require('./routes/books');
+const authRouter = require('./routes/auth');
+const { notFound } = require('./middleware/errorhandler/api-error');
 require('dotenv').config();
 const port = process.env.PORT || 4000;
 const app = express();
 
-
+//middleware
 app.use(express.json());
-
+app.use(notFound);
+app.use(errorHandler);
 //connect database
 connectDb();
 
-app.get('/', ()=> {
-    logger.info("Books API")
+app.use('/api/auth', authRouter);
+app.use('/api/books', bookRouter);
+app.get('/', (req, res)=> {
+    res.send('Books API');
+    logger.info("Books API");
 });
 
 app.listen(() => {
-    logger.info(`START: server listening on http:127.0.0.1:${port}`);
+    logger.info(`START: server listening on http://localhost:${port}`);
 })
