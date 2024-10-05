c104537c2ff0622d7f1884ab6f9b8b85b043d23c
Adebisi Olayinka <adebisiolayinka30@gmail.com>
2024-10-05 13:15:45 +0100

    create user model

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 20
+    },
+
+    lastName: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 20
+    },
+
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    }
+})
+
+const User = mongoose.model('users', userSchema);
+
+module.exports = User
