ffbb761cc844a4963022da0218b7e57464a5bb8e
Adebisi Olayinka <adebisiolayinka30@gmail.com>
2024-10-05 13:16:02 +0100

    wrote book routes

---
+++ routes/books.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bookRouter = express.Router();
+const { createBook,
+    getBook,
+    getAllBooks,
+    updateBook,
+    deleteBook} = require('../controllers/bookController');
+const isLoggedIn = require('../middleware/authentication');
+
+bookRouter.post('/createBook', isLoggedIn, createBook);
+bookRouter.get('/getAllBooks', isLoggedIn, getAllBooks);
+bookRouter.get('/getBookById', isLoggedIn, getBook);
+bookRouter.patch('/updateBook', isLoggedIn, updateBook);
+bookRouter.delete('/deleteBook', isLoggedIn, deleteBook);
+
+module.exports = bookRouter;
