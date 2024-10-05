83a5bfbdfead12ea80f30d1a11b670c2e0993de9
Adebisi Olayinka <adebisiolayinka30@gmail.com>
2024-10-05 13:15:16 +0100

    not found middleware

---
+++ middleware/notFound.js
@@ -0,0 +1,26 @@
+const { isValid } = require('../utils/jwtUtils');
+const isLoggedIn = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+
+
+    if (!authHeader){
+        return next(ApiError.badRequest('unauthorized'))
+    }
+
+    if (authHeader.startsWith('Bearer')){
+
+        const token = authHeader.split(' ')[1]
+
+        try{
+            const payload = isValid(token)
+            req.user = {userId: payload.id}
+            next()
+        }catch(error){
+            return next(ApiError.badRequest('Authentication failed'))
+        }
+    }else{
+        return next(ApiError.badRequest('Invalid authorization header'))
+    }
+}
+
+export default isLoggedIn
